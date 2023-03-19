b43d28a65124814de303fc287eb0d6e8dedbd3dd
tatanoi <ta.phasin@gmail.com>
2023-03-19 23:22:12 +0700

    instance_pizza

---
+++ pizzainstant.py
@@ -0,0 +1,25 @@
+class ProductStock():
+    def __init__(self, product_name, quantity_in_stock):
+        self.product_name = product_name
+        self.quantity_in_stock = quantity_in_stock
+
+Rosa_Grande_Lil_Roni_Cups = ProductStock(ProductType.PIZZA, "Rosa Grande Lil Roni Cups", 50)
+Kindness_Pepperoni = ProductStock(ProductType.PIZZA,"Kindness Pepperoni", 50)
+Classic_Cheese = ProductStock(ProductType.PIZZA,"Classic Cheese", 50)
+Pepperoni = ProductStock(ProductType.PIZZA,"Pepperoni", 50)
+Seafood_Deluxe = ProductStock(ProductType.PIZZA,"Seafood Deluxe", 50)
+Ham_and_mushroom = ProductStock(ProductType.PIZZA,"Ham & Mushroom", 50)
+Hawaiian = ProductStock(ProductType.PIZZA,"Hawaiian", 50)
+Supreme = ProductStock(ProductType.PIZZA,"Supreme ", 50)
+BBQ_Chicken = ProductStock(ProductType.PIZZA,"BBQ Chicken", 50)
+Four_Cheese = ProductStock(ProductType.PIZZA,"Four Cheese", 50)
+Meatzilla = ProductStock(ProductType.PIZZA,"Meatzilla", 50)
+Capricciosa = ProductStock(ProductType.PIZZA,"Capricciosa", 50)
+Parma_Ham = ProductStock(ProductType.PIZZA,"Parma Ham", 50)
+The_White = ProductStock(ProductType.PIZZA,"The White", 50)
+Ricotta_and_Spinach = ProductStock(ProductType.PIZZA,"Ricotta & Spinach", 50)
+Genovese = ProductStock(ProductType.PIZZA,"Genovese", 50)
+Salami_and_Sausage = ProductStock(ProductType.PIZZA,"Salami & Sausage", 50)
+Erotica = ProductStock(ProductType.PIZZA,"Erotica", 50)
+Bolognese = ProductStock(ProductType.PIZZA,"Bolognese", 50)
+Mushroom_and_Artichoke = ProductStock(ProductType.PIZZA,"Mushroom & Artichoke", 50)
